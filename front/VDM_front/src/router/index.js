48ea02838e63fd050a07d3efc4f8ee502ed9b6ce
Vincent4222 <vincent.bourgin@hotmail.fr>
2025-03-12 09:52:57 +0100

    majfonctionnel

--- front/VDM_front/src/router/index.js
+++ front/VDM_front/src/router/index.js
@@ -1,23 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// npm install vue-router
+import VDMHome from '@/views/VDMHome.vue'
+import VDMDetail from '@/views/VDMDetail.vue'
+import VDMPanier from '@/views/VDMPanier.vue'
+
+const routes = [
+  { path: '/', component: VDMHome },
+  { path: '/detail', component: VDMDetail },
+  { path: '/panier', component: VDMPanier }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
---
+++ front/VDM_front/src/views/VDMHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <div v-if="meubles_anciens.length">
+      <div v-for="(meuble, index) in meubles_anciens" :key="index">
+        <p>Nom {{ meuble.nom }}</p>
+
+      </div>
+    </div>
+  </div>
+</template>
+<!-- Crée un dossier SERVICE qui contiendra les fetch -->
+<script>
+export default {
+  data() {
+    return {
+      meubles_anciens: [],
+    };
+  },
+  async mounted() {
+    const response = await fetch("/meubles_anciens.json");
+    const data = await response.json();
+    this.meubles_anciens = data.meubles;
+  },
+};
+</script>
