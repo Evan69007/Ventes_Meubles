e969907b6138e18e9e81f4653d7e0ac82165a6d6
Elo-S38 <elodie.sorce@gmail.com>
2025-03-12 15:13:07 +0100

    Update VDMHome.vue

--- front/VDM_front/src/components/FooterComponent.vue
+++ front/VDM_front/src/components/FooterComponent.vue
@@ -1,6 +1,8 @@
 <template>
-  <footer class="fixed bottom-0 left-0 right-0 w-full bg-gray-50 shadow-sm text-brown-800 px-4 py-6">
-    <div class="flex flex-col md:flex-row justify-between items-start md:items-center gap-6 max-w-7xl mx-auto">
+  <footer class="bottom-0 left-0 right-0 w-full bg-gray-50 shadow-sm text-brown-800 px-4 py-6">
+    <div
+      class="flex flex-col md:flex-row justify-between items-start md:items-center gap-6 max-w-7xl mx-auto"
+    >
       <FooterDescriptif class="w-full md:w-1/4" />
       <FooterLiens class="w-full md:w-1/4" />
       <FooterFormNewsletter class="w-full md:w-1/4" />
@@ -15,4 +17,3 @@ import FooterLiens from './FooterLiens.vue'
 import FooterFormNewsletter from './FooterFormNewsletter.vue'
 import FooterLogoPaiement from './FooterLogoPaiement.vue'
 </script>
-
