06088ea7fbf9c28eb6df2183ae9f890b6c279e3d
Theo <theobutz.03@gmail.com>
2025-03-13 09:42:34 +0100

    gestion photos

--- back/ventes_meubles_back/routes/api.php
+++ back/ventes_meubles_back/routes/api.php
@@ -2,6 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MeublesController;
+use App\Http\Controllers\PhotosController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,11 @@ Route::post('/meubles', [MeublesController::class, 'store']);
 Route::get('/meubles/{id}', [MeublesController::class, 'show']);
 Route::put('/meubles/{id}', [MeublesController::class, 'update']);
 Route::delete('/meubles/{id}', [MeublesController::class, 'destroy']);
+
+Route::prefix('meubles/{meubleId}/photos')->group(function () {
+    Route::get('/', [PhotosController::class, 'index']); // Liste des photos
+    Route::post('/', [PhotosController::class, 'store']); // Ajouter une photo
+    Route::get('/{photoId}', [PhotosController::class, 'show']); // Afficher une photo
+    Route::put('/{photoId}', [PhotosController::class, 'update']); // Mettre à jour une photo
+    Route::delete('/{photoId}', [PhotosController::class, 'destroy']); // Supprimer une photo
+});
