9e4982f5827a955e2778ac77da6c0eedb17e8b67
tanguyjo <jonquatanguy@gmail.com>
2025-03-17 15:46:24 +0100

    Ajout de la page Compte

--- front/VDM_front/src/router/index.js
+++ front/VDM_front/src/router/index.js
@@ -7,6 +7,7 @@ import VDMStock from '@/views/VDMStock.vue'
 import VDMCreationCompte from '@/views/VDMCreationCompte.vue'
 import VDMAjoutMeuble from '@/views/VDMAjoutMeuble.vue'
 import VDMUpdateMeuble from '@/views/VDMUpdateMeuble.vue'
+import VDMCompte from '@/views/VDMCompte.vue'
 
 const routes = [
   { path: '/', component: VDMHome },
@@ -17,6 +18,7 @@ const routes = [
   { path: '/creation-compte', component: VDMCreationCompte },
   { path: '/ajout-meuble', component: VDMAjoutMeuble },
   { path: '/update-meuble', component: VDMUpdateMeuble },
+  { path: '/compte', component: VDMCompte },
 ]
 
 const router = createRouter({
---
+++ front/VDM_front/src/views/VDMCompte.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Compte from '@/components/Compte/MonCompte.vue'
+</script>
+
+<template>
+  <div class="pageContainer">
+    <Compte />
+  </div>
+</template>
+
+<style scoped>
+.pageContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: black;
+}
+</style>
