80d2d0ddb546ac68c27ca0611d60e9460b8a6ca3
askri bilal <askri.bilal@hotmail.fr>
2025-03-17 16:15:17 +0100

    bouton deconnexion activé

--- front/VDM_front/src/components/NavBar/VDMIcons.vue
+++ front/VDM_front/src/components/NavBar/VDMIcons.vue
@@ -5,6 +5,7 @@ import CartIcon from '../icons/IconCart.vue'
 
 <template>
   <div class="icons">
+    <router-link to="/connexion"><UserIcon /> Deconnexion</router-link>
     <router-link to="/compte"><UserIcon /> Mon compte</router-link>
     <router-link to="/panier"><CartIcon /> Mon panier</router-link>
   </div>
