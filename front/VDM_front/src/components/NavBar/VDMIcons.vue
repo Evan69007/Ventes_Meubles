457b78fda661fc315fd27ac561421b06b6e2cbe9
askri bilal <askri.bilal@hotmail.fr>
2025-03-17 16:06:09 +0100

    page mon compte lié

--- front/VDM_front/src/components/NavBar/VDMIcons.vue
+++ front/VDM_front/src/components/NavBar/VDMIcons.vue
@@ -5,7 +5,7 @@ import CartIcon from '../icons/IconCart.vue'
 
 <template>
   <div class="icons">
-    <router-link to=""><UserIcon /> Mon compte</router-link>
+    <router-link to="/compte"><UserIcon /> Mon compte</router-link>
     <router-link to="/panier"><CartIcon /> Mon panier</router-link>
   </div>
 </template>
